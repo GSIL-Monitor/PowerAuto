6d33e2908c5e7e711eeb29325032960bf02b9d8a
dicky <love94831@vip.qq.com>
2017-10-16 18:57:37 +0800

    初始化

---
+++ action/PowerAutoAction.php
@@ -0,0 +1,85 @@
+﻿<?php
+header("Content-type: text/html; charset=utf-8");
+
+
+
+error_reporting(E_ALL);
+//User name off the mail box
+$username = 's125hckj@ooopic.com';
+//Password of mailbox
+$password = 'Hdy123456';
+//Email address of that mailbox some time the uname and email address are identical
+$email_address = 's125hckj@ooopic.com';
+//Ip or name of the POP or IMAP mail server
+$resmail_server = '{pop.exmail.qq.com:143/imap}INBOX';
+$mail_server = 'pop.exmail.qq.com';
+//if this server is imap or pop default is pop
+$server_type = 'imap'; 
+//Server port for pop or imap Default is 110 for pop and 143 for imap
+$port = 143;
+$st= microtime(true);
+//$mail = new mail($username,$password,$email_address,$mail_server,$server_type,$port);
+//$mail->connect();
+//echo $mail_count=$mail->get_mail_total();
+//echo $mail_str=iconv('gbk','utf-8',$mail->get_body($mail_count-7));
+//$mail_str='
+//1287<div style="min-height:22px;margin-bottom:8px;"><div style="min-height:22px;margin-bottom:8px;">邮件类型:开通权限</div><div style="min-height:22px;margin-bottom:8px;"><br></div><div style="min-height:22px;margin-bottom:8px;">开通用户:黄德银</div><div style="min-height:22px;margin-bottom:8px;"><br></div><div style="min-height:22px;margin-bottom:8px;">日期:2017年10月15日16:20:26</div><div style="min-height:22px;margin-bottom:8px;"><br></div><div style="min-height:22px;margin-bottom:8px;">申请开通权限列表:</div><div style="min-height:22px;margin-bottom:8px;"><br></div><div style="min-height:22px;margin-bottom:8px;">老后台{</div><div style="min-height:22px;margin-bottom:8px;"><br></div><div style="min-height:22px;margin-bottom:8px;">服务会员|读取</div><div style="min-height:22px;margin-bottom:8px;"><br></div><div style="min-height:22px;margin-bottom:8px;">汇款会员|读取</div><div style="min-height:22px;margin-bottom:8px;"><br></div><div style="min-height:22px;margin-bottom:8px;">新卖家审核|读取</div><div style="min-height:22px;margin-bottom:8px;"><br></div><div style="min-height:22px;margin-bottom:8px;">店铺权限管理|操作</div><div style="min-height:22px;margin-bottom:8px;"><br></div><div style="min-height:22px;margin-bottom:8px;">会员提现记录|读取</div><div style="min-height:22px;margin-bottom:8px;"><br></div><div style="min-height:22px;margin-bottom:8px;">}</div><div style="min-height:22px;margin-bottom:8px;"><br></div><div style="min-height:22px;margin-bottom:8px;">新后台{</div><div style="min-height:22px;margin-bottom:8px;"><br></div><div style="min-height:22px;margin-bottom:8px;">VIP分类销售统计|操作</div><div style="min-height:22px;margin-bottom:8px;"><br></div><div style="min-height:22px;margin-bottom:8px;">PPT分类销售统计|操作</div><div style="min-height:22px;margin-bottom:8px;"><br></div><div style="min-height:22px;margin-bottom:8px;">SEM推广数据|操作</div><div style="min-height:22px;margin-bottom:8px;"><br></div><div style="min-height:22px;margin-bottom:8px;">}</div></div><div id="original-content"></div>';
+$mail_str='
+1287<html><head><meta http-equiv="content-type" content="text/html; charset=GB2312"><style>body { line-height: 1.5; }blockquote { margin-top: 0px; margin-bottom: 0px; margin-left: 0.5em; }div.foxdiv20171013172330739293 { }body { font-size: 10.5pt; font-family: 微软雅黑; color: rgb(0, 0, 0); line-height: 1.5; }</style></head><body>
+<div><span></span><br></div>
+<div><br></div><hr style="width: 210px; height: 1px;" color="#b5c4df" size="1" align="left">
+<div><span><div style="MARGIN: 10px; FONT-FAMILY: verdana; FONT-SIZE: 10pt"><div>s98hckj@ooopic.com</div></div></span></div>
+<blockquote style="margin-top: 0px; margin-bottom: 0px; margin-left: 0.5em;"><div>&nbsp;</div><div style="border:none;border-top:solid #B5C4DF 1.0pt;padding:3.0pt 0cm 0cm 0cm"><div style="PADDING-RIGHT: 8px; PADDING-LEFT: 8px; FONT-SIZE: 12px;FONT-FAMILY:tahoma;COLOR:#000000; BACKGROUND: #efefef; PADDING-BOTTOM: 8px; PADDING-TOP: 8px"><div><b>发件人：</b>&nbsp;<a href="mailto:S282hckj@ooopic.com">张明敏</a></div><div><b>发送时间：</b>&nbsp;2017-10-13&nbsp;17:18</div><div><b>收件人：</b>&nbsp;<a href="mailto:s98hckj@ooopic.com">魏飞</a></div><div><b>抄送：</b>&nbsp;<a href="mailto:s82hckj@ooopic.com">陈庆捷</a>; <a href="mailto:zjy@ooopic.com">张家玉</a>; <a href="mailto:cohckj@ooopic.com">我图内容</a>; <a href="mailto:S286hckj@ooopic.com">王立厂</a>; <a href="mailto:pmhckj@ooopic.com">我图PM</a></div><div><b>主题：</b>&nbsp;回复: 问卷调研需求（紧急）</div></div></div><div><div class="FoxDiv20171013172330739293">
+<div style="font-size: 19px;"><b><font color="#ff0000">该需求比较紧急，辛苦飞哥排期，今天下班前完成上线</font></b></div>
+<div><br></div><hr style="width: 210px; height: 1px;" color="#b5c4df" size="1" align="left">
+<div><span><div style="MARGIN: 10px; FONT-FAMILY: verdana; FONT-SIZE: 10pt"><div><div>姓名：张明敏</div><div>职位：产品</div></div><div><div><a href="mailto:S167hckj@ooopic.com" style="text-decoration: none !important; font-size: 10pt; line-height: 1.5; background-color: window;">S282hckj@ooopic.com</a><span style="color: rgb(0, 0, 0); font-size: 10pt; line-height: 1.5; background-color: rgba(0, 0, 0, 0);">&nbsp;</span></div><div><img src="cid:_Foxmail.1@e231ac2b-2e57-33f1-f7f5-cc4d797662fb" border="0" style="max-width: 100%; height: auto !important;"></div></div></div></span></div>
+<blockquote style="margin-top: 0px; margin-bottom: 0px; margin-left: 0.5em;"><div>&nbsp;</div><div style="border:none;border-top:solid #B5C4DF 1.0pt;padding:3.0pt 0cm 0cm 0cm"><div style="PADDING-RIGHT: 8px; PADDING-LEFT: 8px; FONT-SIZE: 12px;FONT-FAMILY:tahoma;COLOR:#000000; BACKGROUND: #efefef; PADDING-BOTTOM: 8px; PADDING-TOP: 8px"><div><b>发件人：</b>&nbsp;<a href="mailto:S286hckj@ooopic.com">王立厂</a></div><div><b>发送时间：</b>&nbsp;2017-10-13&nbsp;16:48</div><div><b>收件人：</b>&nbsp;<a href="mailto:s282hckj@ooopic.com">张明敏</a></div><div><b>抄送：</b>&nbsp;<a href="mailto:s82hckj@ooopic.com">陈庆捷</a>; <a href="mailto:zjy@ooopic.com">张家玉</a>; <a href="mailto:cohckj@ooopic.com">我图内容</a></div><div><b>主题：</b>&nbsp;问卷调研需求（紧急）</div></div></div><div><div class="FoxDiv20171013171738264321">
+<div style="font-size: 15px;"><span></span>dear 明敏，</div><div style="font-size: 15px;">&nbsp; &nbsp; 针对“卖家问卷调研”，有2个需求：</div><div style="font-size: 15px;">&nbsp; &nbsp; 需求一：<span style="background-color: rgba(0, 0, 0, 0); line-height: 1.5;">卖家登陆<font color="#ff0000">卖家后台，弹窗提醒</font>参与问卷填写，在卖家参与问卷填写后不再弹出 &nbsp; 文案可参考“参与我图卖家有奖问卷调研”</span></div><div style="font-size: 15px;">&nbsp; &nbsp; 需求二：<font color="#ff0000">问卷入口的前台展示</font>（目的是为了提高卖家的参与率）：<b>仅针对卖家账户登陆显示，</b>在网站首页、搜索页、分类页、详情页的如下位置，加上一个悬浮通知，<span style="line-height: 1.5; background-color: window;">位置如下图红框处，具体文案可参考下图，</span><span style="background-color: window;">在卖家参与问卷填写后不再显示，</span><span style="background-color: window; line-height: 1.5;">具体实现效果最好设计也能配合下。</span></div><div style="font-size: 15px;"><span style="background-color: window; line-height: 1.5;">&nbsp; &nbsp;落地页为：</span><span style="line-height: 1.5; background-color: window;">https://www.wjx.cn/jq/17120934.aspx</span></div><div style="font-size: 15px;"><span style="line-height: 1.5; background-color: window;">&nbsp; &nbsp; 以上2个需求，</span><span style="color: rgb(0, 0, 0); background-color: rgba(0, 0, 0, 0); line-height: 1.5;">上线时间为</span><span style="background-color: rgba(0, 0, 0, 0); line-height: 1.5;"><font color="#ff0000">2017/10/13（今日） 17:30</font></span><span style="color: rgb(0, 0, 0); background-color: rgba(0, 0, 0, 0); line-height: 1.5;"> ,下线时间预期</span><span style="color: rgb(0, 0, 0); background-color: rgba(0, 0, 0, 0); line-height: 1.5;">2017/10/20 17:00，</span><span style="background-color: rgba(0, 0, 0, 0); line-height: 1.5;"><font color="#ff0000"><b>需要加急！！</b></font></span></div><div style="font-size: 15px;"><div>
+<img src="cid:_Foxmail.1@b3505d04-2753-c664-3d19-e701b7ac448f"></div></div><div style="font-size: 15px;"><span style="background-color: rgba(0, 0, 0, 0); line-height: 1.5;"><font color="#ff0000"><b>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</b></font></span></div><div><div>
+&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</div></div>
+<div><br></div><hr style="width: 210px; height: 1px;" color="#b5c4df" size="1" align="left">
+<div><span><div style="MARGIN: 10px; FONT-FAMILY: verdana; FONT-SIZE: 10pt"><div><span style="font-size: 10pt; color: rgb(0, 0, 0); line-height: 1.5; background-color: rgba(0, 0, 0, 0);">&nbsp;</span></div><div><div><div>姓名：王立厂</div><div>职位：内容运营</div></div><div><div><a href="mailto:S286hckj@ooopic.com" style="font-size: 10pt; line-height: 1.5; background-color: window;">S286hckj@ooopic.com</a><span style="color: rgb(0, 0, 0); font-size: 10pt; line-height: 1.5; background-color: rgba(0, 0, 0, 0);">&nbsp;</span></div><div><img src="cid:_Foxmail.1@3ad9cfde-1cf5-d225-b372-cc7bb573beb9" border="0" style="max-width: 100%; height: auto !important;"></div></div></div><span style="color: rgb(0, 0, 0); background-color: rgba(0, 0, 0, 0);"></span></div></span></div>
+</div></div></blockquote>
+</div></div></blockquote>
+</body></html>
+';
+echo '<br>获取邮件用时:' . (microtime(true) - $st) . '<br>';
+$st2=microtime(true);
+if(true || stripos($mail_str,'开通权限')!==false){
+     $mail_str=str_ireplace('/<br>/','\n',$mail_str);
+     $mail_str=preg_replace('/<style.*?>.*?<\/style>|<script.*?>.*?<\/script>|<[a-z]{1,}.*?>|<\/[a-z]{1,}>/','',$mail_str);
+    echo $mail_str=str_ireplace('/\\n/','<br>',$mail_str);
+    echo 'ok';
+    echo '<br>程序处理用时:' . (microtime(true) - $st2) . '<br>';
+    exit;
+    if(preg_match('/开通用户(.*?)<br>/',$mail_str,$result)){
+        var_dump($result);
+    }else{
+        echo '开通用户格式错误';
+    }
+}else{
+    echo '非开通权限邮件';
+}
+
+//$mail = imap_open($mail_server, $username, $password);
+//echo $mail_count = imap_num_msg($mail);
+//echo $mail_str = iconv('gbk','utf-8',imap_body($mail, $mail_count));
+//$mail_structure=imap_fetchstructure($mail,$mail_count);
+// print_r($mail_structure->parts);
+//$mail_body=imap_fetchbody($mail,$mail_count,1);
+//var_dump($mail_body);
+//echo $mail_str=imap_qprint($mail_body);
+//var_dump(iconv('gb2312','utf-8',$mail_str));
+//echo $mail_structure->type.'/'.$mail_structure->subtype;
+//echo "<br>";
+//echo $mail_structure->encoding;
+//exit;
+//var_dump($mail_structure);
+//
+//$class = new ReflectionClass($mail_structure);//建立 Person这个类的反射类
+//Reflection::export(new ReflectionClass($mail_structure));
+//$methods=$class->getMethods();
+//var_dump($methods);
+//$attr=$class->getProperties();
+//var_dump($attr);
+//var_dump($mail_structure);
