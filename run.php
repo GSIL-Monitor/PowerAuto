303252ec048d4b097fd1de5b457eb8b0986a95e3
dicky <love94831@vip.qq.com>
2017-10-16 19:46:27 +0800

    初始化

--- run.php
+++ run.php
@@ -1,5 +1,8 @@
 <?php
 namespace PowerAuto;
 $m=isset($_GET['m'])?strtolower($_GET['m']):'Index';
+
 $a=isset($_GET['a'])?strtolower($_GET['a']):'index';
+
 require './action/' . $m . 'Action.php';
+
